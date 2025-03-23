6323b3db1bf646e7fa7e214233d9392177fbad01
akoent <akoent7666@yahoo.com>
2025-03-22 19:26:52 -0500

    Update capture_data.php

--- capture_data.php
+++ capture_data.php
@@ -1,7 +1,8 @@
 <html>
 <body>
+<h3>Get Your Organic Juice Offer!</h3>
 First Name <?php echo $_POST[“first_name”; ?><br>
 Last Name <?php echo $_POST[“last_name”; ?><br>
 Email Address <?php echo $_POST[“email”; ?><br>
 </body>
-</html
+</html>
